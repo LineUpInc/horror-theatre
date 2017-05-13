601a9de25c1dc70d6f3dda39d7cbb058e40c1e15
Konstantin Musienko <kmusienko97@gmail.com>
2017-05-13 19:47:53 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,19 @@
+<?php
+        $to = 'kmusienko97@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <test@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+        echo '<script type="text/javascript">alert("We will call you soon");</script>'; 
+        header("Location: https://horror-theatre.000webhostapp.com"); 
+?>
